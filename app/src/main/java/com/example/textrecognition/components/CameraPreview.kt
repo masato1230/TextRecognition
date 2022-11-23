6659027c8979b4cf0ed2973d9cfa09b080c1ed98
masato1230 <0ht3851f11t426k@ezweb.ne.jp>
2022-11-23 18:32:20 +0900

    Add CameraPreview component

---
+++ app/src/main/java/com/example/textrecognition/components/CameraPreview.kt
@@ -0,0 +1,82 @@
+package com.example.textrecognition.components
+
+import android.content.Context
+import android.util.Log
+import android.view.ViewGroup
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat
+import kotlinx.coroutines.launch
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
+import kotlin.coroutines.resume
+import kotlin.coroutines.suspendCoroutine
+
+@Composable
+fun CameraPreview(modifier: Modifier, onQrDetected: (code: String) -> Unit) {
+    val coroutineScope = rememberCoroutineScope()
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    AndroidView(
+        modifier = modifier,
+        factory = { context ->
+            val previewView = PreviewView(context).apply {
+                this.scaleType = scaleType
+                layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.MATCH_PARENT
+                )
+            }
+            // CameraX Preview UseCase
+            val previewUseCase = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(previewView.surfaceProvider)
+                }
+
+            // QRコード解析UseCase
+            val qrCodeAnalysisUseCase = ImageAnalysis.Builder()
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .build()
+                .also {
+                    it.setAnalyzer(
+                        Executors.newSingleThreadExecutor(),
+                        QRCodeAnalyzer(onQrDetected)
+                    )
+                }
+
+            coroutineScope.launch {
+                val cameraProvider = context.getCameraProvider()
+                try {
+                    // use caseをライフサイクルにバインドする前にアンバインドを行う必要がある
+                    cameraProvider.unbindAll()
+                    cameraProvider.bindToLifecycle(
+                        lifecycleOwner, CameraSelector.DEFAULT_BACK_CAMERA, previewUseCase, qrCodeAnalysisUseCase
+                    )
+                } catch (ex: Exception) {
+                    Log.e("CameraPreview", "Use case binding failed", ex)
+                }
+            }
+            previewView
+        }
+    )
+}
+
+suspend fun Context.getCameraProvider(): ProcessCameraProvider = suspendCoroutine { continuation ->
+    ProcessCameraProvider.getInstance(this).also { future ->
+        future.addListener({
+            continuation.resume(future.get())
+        }, executor)
+    }
+}
+
+val Context.executor: Executor
+    get() = ContextCompat.getMainExecutor(this)
